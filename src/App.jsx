3b164b74670ea192dda7fc719bc56129d97e7194
Joaquin <joaquin.astengo@gmail.com>
2022-02-26 02:22:04 -0300

    añadiendo clientes a la tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './layout/Layout'
 import Inicio from './paginas/Inicio'
+import VerCliente from './paginas/VerCliente'
 import EditarCliente from './paginas/EditarCliente'
 import NuevoCliente from './paginas/NuevoCliente'
 
@@ -13,7 +14,8 @@ function App() {
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
           <Route path="nuevo" element={<NuevoCliente />} />
-          <Route path="editar/:id" elemet={<EditarCliente />} />
+          <Route path="editar/:id" element={<EditarCliente />} />
+          <Route path=":id" element={<VerCliente />} />
         </Route>
 
       </Routes>
