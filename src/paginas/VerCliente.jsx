72bb0176bb46d7f4d7fb07277a1ebc94b6867340
Joaquin <joaquin.astengo@gmail.com>
2022-02-26 03:04:00 -0300

    agregando spinner

--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -1,15 +1,81 @@
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Spinner from '../components/Spinner'
+
 
 const VerCliente = () => {
 
-  const {id} = useParams()
+  const [cliente, setCliente] = useState({})
+  const [cargando, setCargando] = useState(true);
 
-  console.log(id)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setCliente(resultado)
+      } catch (error) {
+        console.log(error)
+      }
+      setCargando(false)
+    }
+    obtenerClienteAPI()
+  }, [])
+
 
   return (
-    <div>
-      <h1>VerCliente.jsx</h1>
-    </div>
+
+
+    cargando ? <Spinner /> :
+      Object.keys(cliente).length === 0 ?
+        <p>No hay resultados</p> :
+
+        (
+          <div>
+            <>
+              <h1 className="font-black text-4xl text-blue-900">Ver Cliente {cliente.nombre}</h1>
+              <p className="mt-3">Información del Cliente</p>
+
+
+              <p className='text-4xl text-gray-600 mt-10'>
+                <span className=' text-gray-800 uppercase font-bold '>Cliente: </span>
+                {cliente.nombre}
+              </p>
+              <p className='text-2xl mt-4 text-gray-600'>
+                <span className=' text-gray-800 uppercase font-bold '>Email: </span>
+                {cliente.email}
+              </p>
+              {cliente.telefono && (
+                <p className='text-2xl mt-4 text-gray-600'>
+                  <span className=' text-gray-800 uppercase font-bold '>Teléfono: </span>
+                  {cliente.telefono}
+                </p>
+              )}
+
+              <p className='text-2xl mt-4 text-gray-600'>
+                <span className=' text-gray-800 uppercase font-bold '>Empresa: </span>
+                {cliente.empresa}
+              </p>
+              {cliente.notas && (
+                <p className='text-2xl mt-4 text-gray-600'>
+                  <span className=' text-gray-800 uppercase font-bold '>Notas: </span>
+                  {cliente.notas}
+                </p>
+              )}
+            </>
+
+
+
+
+          </div>
+        )
+
+
   )
 }
 
